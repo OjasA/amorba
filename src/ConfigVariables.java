a33ddf6a2ab2d77ede98a5e6011d109c298326f2
Ojas A <kingbobthefifth@gmail.com>
2015-05-14 15:34:34 -0500

    .gitignore changes

--- src/ConfigVariables.java
+++ src/ConfigVariables.java
@@ -1,5 +1,9 @@
+import java.awt.Color;
 
 public class ConfigVariables {
-	public boolean keyboard = false;
-	
+	public static boolean keyboard = false;
+	public static Color backgroundColor = Color.gray;
+	public static String playerName = "Amorba";
+	public static Color playerColor = Color.blue;
+
 }
--- src/Window.java
+++ src/Window.java
@@ -1,20 +1,18 @@
 import javax.swing.JFrame;
 
+public class Window extends JFrame {
 
-public class Window extends JFrame{
-
-	private Window()
-	{
+	private Window() {
 		this.setTitle("Game");
 		this.setSize(600, 800);
-        this.setLocationRelativeTo(null);
-        this.setResizable(false);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
-        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        //this.setContentPane(new Game()); //main game class
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		// this.setContentPane(new Game()); //main game class
 	}
-	
+
 	public static void main(String[] args) {
 		new Window();
 	}
