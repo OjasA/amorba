35c00bec9f7ac1f6ae5b4dc44b2b8c43ce6b7339
Kevin Black <kevin3.black@gmail.com>
2015-05-14 16:56:42 -0500

    Cleaned up issues with Winodw

--- src/Window.java
+++ src/Window.java
@@ -4,8 +4,6 @@ import javax.swing.JFrame;
 
 public class Window extends JFrame {
 
-<<<<<<< HEAD
-public class Window extends JFrame{
 
 	private Window()
 	{
@@ -17,20 +15,10 @@ public class Window extends JFrame{
         this.setVisible(true);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setContentPane(new Menu()); //main game class
+	} 
         
-        
-=======
-	private Window() {
-		this.setTitle("Game");
-		this.setSize(600, 800);
-		this.setLocationRelativeTo(null);
-		this.setResizable(false);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true);
-		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		// this.setContentPane(new Game()); //main game class
->>>>>>> origin/master
-	}
+
+	
 
 	public static void main(String[] args) {
 		new Window();
