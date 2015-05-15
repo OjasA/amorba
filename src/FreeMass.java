0519c8ecf7230d01f9a2c6d3cf0290be5f8444af
AbhiDhir <dhir.abhimanyu@gmail.com>
2015-05-15 14:57:52 -0500

    minor edits for color

--- src/FreeMass.java
+++ src/FreeMass.java
@@ -1,9 +1,12 @@
 import java.awt.Point;
+import java.awt.Color;
 
 public class FreeMass extends Cell {
+	private Color color;
 	
-	public FreeMass(Point theLocation, int xSpeed, int ySpeed) {
+	public FreeMass(Point theLocation, int xSpeed, int ySpeed, Color col) {
 		super(20, theLocation, xSpeed, ySpeed);
+		color = col;
 	}
 	
 
--- src/Player.java
+++ src/Player.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 import java.awt.Point;
 
 public class Player extends Cell {
-	Color color;
+	 private Color color;
 
 	public Player(int theSize, Point theLocation, Color theColor) {
 		super(theSize, theLocation);
