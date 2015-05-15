540003135ec808b0e6f182e81043fc7e29df751d
Ojas A <kingbobthefifth@gmail.com>
2015-05-15 15:22:01 -0500

    add GPL

--- src/ConfigVariables.java
+++ src/ConfigVariables.java
@@ -1,10 +1,24 @@
 import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConfigVariables {
+
+	private String configLocation = System.getProperty("user.home")
+			+ "\\.amorba\\";
+	HashMap<String, String> configs = new HashMap<String, String>();
 	public static boolean keyboard = false;
 	public static Color backgroundColor = Color.gray;
 	public static String playerName = "Amorba";
 	public static Color playerColor = Color.blue;
 	public static double mapScale = 1.0; // scales map according to player size
 	public static int size = 20; // default size of particles
+
+	public void writeConfig() {
+
+	}
+
+	private void readConfig() {
+
+	}
 }
