a4c458b8449af93a9d3b8d510929192d5de0cd37
Harrison Tran <atxharrison.tran@gmail.com>
2015-05-15 21:26:11 -0500

    Add mitosis method

--- src/AbstractDraw.java
+++ src/AbstractDraw.java
@@ -1,14 +1,20 @@
+import java.awt.Color;
+import java.awt.Canvas;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import static java.lang.Character.*;
+import java.awt.image.BufferedImage;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
 
-
-public abstract class AbstractDraw extends JPanel{
+public abstract class AbstractDraw extends JPanel {
 	
 	public abstract void draw(Graphics2D g2d);
 	
-	@Override
 	public void paintComponent(Graphics g)
 	{
 		Graphics2D g2d = (Graphics2D) g;
