d83270d6303ce2472cce3b5782442ecdabfd8866
Harrison Tran <s2050075@W7SJ3WMCP1.ad.austinisd.org>
2015-05-19 14:35:39 -0500

    Groundwork for establishing server connection
    Still need to fill out readResponse()

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -1,13 +1,43 @@
 
-import java.io.*;
-import java.util.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
  
 public class DatabaseManager {
+	private String hostname;
+	private int port;
+	Socket socketClient;
+	
+	public DatabaseManager(String hostname, int port){
+		this.hostname = hostname;
+		this.port = port;
+	}
+	
+	public void connect() throws UnknownHostException, IOException {
+		socketClient = new Socket(hostname,port);
+	}
+	
+	public void readResponse() throws IOException{
+		/* INSERT RESPONSE FROM SERVER */
+    }
+	
+	public static void main(String arg[]){
+        //Creating a SocketClient object
+        DatabaseManager client = new DatabaseManager ("localhost",9990);
+        try {
+            //trying to establish connection to the server
+            client.connect();
+            //if successful, read response from server
+            client.readResponse();
 
-	public static void main(String[] args) {
-		String host = "127.0.0.1";
-		int port = 0;
-		
+        } catch (UnknownHostException e) {
+            System.err.println("Host unknown. Cannot establish connection");
+        } catch (IOException e) {
+            System.err.println("Cannot establish connection. Server may not be up."+e.getMessage());
+        }
 	}
 }
