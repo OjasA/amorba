0fdc9b61ba3dbf01d545c5b31fbb903f9d055544
Harrison Tran <atxharrison.tran@gmail.com>
2015-05-22 19:42:58 -0500

    Show the player name
    Font scales with player! Also I'll need to go back to that camera thing
    again to make sure it scales properly, screws up after about 150.

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,13 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Font;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
 
 public class Player extends Cell {
 
@@ -18,5 +25,16 @@ public class Player extends Cell {
 		
 		g2d.setColor(super.getColor());
 		g2d.fillOval((int) super.getLocation().getX() - super.getRadius(), (int) super.getLocation().getY() - super.getRadius(), super.getRadius() * 2, super.getRadius() * 2); //draws new oval
+		
+		g2d.setColor(Color.WHITE);
+		String name = new String("Amorba!!!");
+		AffineTransform affinetransform = new AffineTransform();    
+		FontRenderContext frc = new FontRenderContext(affinetransform,true,true);     
+		Font font = new Font("Trebuchet MS", Font.PLAIN, (int) (this.getRadius()*0.4));
+		int textwidth = (int)(font.getStringBounds(name, frc).getWidth());
+		int textheight = (int)(font.getStringBounds(name, frc).getHeight());
+		g2d.setFont(font);
+		g2d.drawString(name,(int)this.getLocation().getX()-(textwidth/2),(int)this.getLocation().getY()+(textheight/4));
+		
 	}
 }
